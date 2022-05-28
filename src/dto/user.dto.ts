368380016eb3504617412cf2eba9bc91d0960967
george <heohii.pustovit@ava.codes>
2022-05-28 23:12:52 +0300

    [BT-2] fix imports order

--- src/dto/user.dto.ts
+++ src/dto/user.dto.ts
@@ -1,5 +1,6 @@
-import { UserType, UserForReturnType } from '../interfaces/user.interface';
 import { omit } from 'lodash';
+
+import { UserType, UserForReturnType } from '../interfaces/user.interface';
 
 export const userDto = (user: UserType): UserForReturnType => {
   return omit(user, ['password', 'refreshToken']);
