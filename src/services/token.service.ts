cf3a5fe97e521b03d50b0b60b087fbb3f2f38ac8
george <heohii.pustovit@ava.codes>
2022-05-28 23:26:24 +0300

    [BT-2] create token.service.ts

---
+++ src/services/token.service.ts
@@ -0,0 +1,79 @@
+import jwt, { JwtPayload } from 'jsonwebtoken';
+import UUID from 'uuid';
+
+import Token from '../models/Token.model';
+import ApiError from '../exceptions/api-errors';
+
+export const verifyAccess = (accessToken: string): JwtPayload | string => {
+  try {
+    return jwt.verify(accessToken, process.env.JWT_ACCESS_SECRET!);
+    // @ts-ignore
+  } catch (error: Error) {
+    throw ApiError.BadRequest(error.message!, '');
+  }
+};
+
+export const verifyRefresh = (refreshToken: string): JwtPayload | string => {
+  try {
+    return jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET!);
+    // @ts-ignore
+  } catch (error: Error) {
+    throw ApiError.BadRequest(error.message, '');
+  }
+};
+
+const generateAccessToken = (userId: string): string => {
+  return jwt.sign(
+    { userId, type: 'access' },
+    process.env.JWT_ACCESS_SECRET!,
+    { expiresIn: '15m' },
+  );
+};
+
+type generateRefreshTokenType = {
+  token: string
+  id: string
+}
+
+const generateRefreshToken = (): generateRefreshTokenType => {
+  const uuid = UUID.v4();
+  return {
+    token: jwt.sign(
+      { id: uuid, type: 'refresh' },
+      process.env.JWT_REFRESH_SECRET!,
+      { expiresIn: '30d' }),
+    id: uuid,
+  };
+};
+
+const replaceDbRefreshToken = async (
+  {
+    newTokenId, oldTokenId, userId, update,
+  }: {newTokenId: string, oldTokenId: string, userId: string, update: boolean}) => {
+  if (update) {
+    await Token.findOneAndRemove({ tokenId: oldTokenId });
+  }
+
+  return Token.create({ tokenId: newTokenId, userId });
+};
+
+export type updateTokensReturnType = {
+  accessToken: string
+  refreshToken: string
+  accessExpiration: number
+}
+
+export const updateTokens = async (userId: string, tokenId: string = '', update = false): Promise<updateTokensReturnType> => {
+  const accessToken: string = generateAccessToken(userId);
+  const refreshToken: generateRefreshTokenType = generateRefreshToken();
+  const accessExpiration: number = new Date(new Date().getTime() + 15 * 60000).valueOf();
+
+  await replaceDbRefreshToken({
+    newTokenId: refreshToken.id,
+    oldTokenId: tokenId,
+    userId,
+    update,
+  });
+
+  return { accessToken, refreshToken: refreshToken.token, accessExpiration };
+};
