f3ae31e8546bc088ab3d2bee51e2b5e873ff052c
george <heohii.pustovit@ava.codes>
2022-06-02 20:21:06 +0300

    [BT-4] create balance model

---
+++ src/models/Balance.model.ts
@@ -0,0 +1,12 @@
+import { Document, model, Schema, Types } from 'mongoose';
+
+import { balanceType } from '../types/balance.type';
+
+const schema: Schema = new Schema({
+  name: { type: String, required: true },
+  amount: { type: Number, required: true },
+  ownerId: { type: Types.ObjectId, ref: 'User', required: true },
+});
+
+type MongooseUser = Document & balanceType;
+export default model<MongooseUser>('Balance', schema);
