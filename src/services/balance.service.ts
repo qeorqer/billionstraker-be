7239d4bfc2eae85ecaba8703dac98c84f0c2006f
george <heohii.pustovit@ava.codes>
2022-06-19 11:26:24 +0300

    [BT-5] add logic for updating and deleting balances

--- src/services/balance.service.ts
+++ src/services/balance.service.ts
@@ -1,6 +1,7 @@
 import Balance from '../models/Balance.model';
 import ApiError from '../exceptions/api-errors';
 import { balanceType } from '../types/balance.type';
+import Category from '../models/Category.model';
 
 export const createBalance = async (
   name: string,
@@ -25,4 +26,25 @@ export const createBalance = async (
 export const getBalances = async (userId: string): Promise<balanceType[]> => {
   const userBalances = await Balance.find({ ownerId: userId });
   return userBalances;
+};
+
+export const updateBalance = async (balanceId: string, balance: balanceType): Promise<balanceType> => {
+  const updatedBalance = await Balance.findByIdAndUpdate(balanceId, { ...balance }, { new: true });
+
+  if (!updatedBalance) {
+    throw ApiError.BadRequest('There is no such balance', '');
+  }
+
+  return updatedBalance;
+};
+
+export const deleteBalance = async (balanceId: string): Promise<string> => {
+  const balanceToDelete = await Balance.findById(balanceId);
+
+  if(!balanceToDelete){
+    throw ApiError.BadRequest('There is no such balance', '');
+  }
+
+  await balanceToDelete.remove();
+  return balanceId;
 };
