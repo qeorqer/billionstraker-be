4ac2dae54dc314c9a27c2973da61c2f3ee2eaf45
george <heohii.pustovit@ava.codes>
2024-03-06 13:56:55 +0000

    Update currency convertion endpoint endpoint

--- src/utils/statistics/convertCurrency.ts
+++ src/utils/statistics/convertCurrency.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import Decimal from 'decimal.js';
 
+const endpointBaseApi =
+  'https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/';
+
 export const convertCurrency = async (
   currencyToConvert: string,
   currencyToConvertFrom: string,
   value: number,
 ): Promise<number> => {
   try {
-    const convertApiRoute = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyToConvertFrom}/${currencyToConvert}.json`;
+    const convertApiRoute = `${endpointBaseApi}${currencyToConvertFrom}/${currencyToConvert}.json`;
     const rateForOneResponse = await axios.get(convertApiRoute);
     const rateForOne = rateForOneResponse.data[currencyToConvert];
 
@@ -15,7 +18,7 @@ export const convertCurrency = async (
   } catch (e) {
     try {
       // In case first request fails, which happens sometimes, use another which is more stable
-      const singleCurrencyRoute = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currencyToConvertFrom}.json`;
+      const singleCurrencyRoute = `${endpointBaseApi}${currencyToConvertFrom}.json`;
       const singleCurrencyResponse = await axios.get(singleCurrencyRoute);
       const rateForOne =
         singleCurrencyResponse.data[currencyToConvertFrom][currencyToConvert];
